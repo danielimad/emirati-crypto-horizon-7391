e2dcd059636af5a5c067e1e0d4df5d96150f9ecf
0xDanielimad <72716286+danielimad@users.noreply.github.com>
2025-08-06 17:16:35 +0400

    create vite config

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+```typescript
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+    open: true,
+  }
+});
+```
