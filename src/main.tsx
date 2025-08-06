835d5e694b04646499b35faf0def4d97bdeced8f
0xDanielimad <72716286+danielimad@users.noreply.github.com>
2025-08-06 17:16:07 +0400

    create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+```typescript
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './style.css';
+
+const rootElement = document.getElementById('app');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+```
